f33694842227c6bc66d75fe3206efc3c5f6f62df
Katleho14 <katlehomotloung394@gmail.com>
2024-11-03 17:53:08 +0200

    "Updated AnalyticsPage imports: fixed typo in ChannelPerformance, added ProductPerformance"

--- src/pages/AnalyticsPage.jsx
+++ src/pages/AnalyticsPage.jsx
@@ -2,7 +2,8 @@ import Header from "../components/common/Header";
 
 import OverviewCards from "../components/analytics/OverviewCards";
 import RevenueChart from "../components/analytics/RevenueChart";
-import ChannelPerformance from "../components/analytics/ChannelPerfomance";
+import ChannelPerformance from "../components/analytics/ChannelPerformance";
+import ProductPerformance from "../components/analytics/ProductPerformance";
 import UserRetention from "../components/analytics/UserRetention";
 import CustomerSegmentation from "../components/analytics/CustomerSegmentation";
 import AIPoweredInsights from "../components/analytics/AIPoweredInsights";
