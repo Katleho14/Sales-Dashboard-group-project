bd40eec991ef4a549669b7c1b34c697dcc00f988
Katleho14 <katlehomotloung394@gmail.com>
2024-11-03 17:50:11 +0200

    "Deleted OrderTable, DailySalesTrend, SalesByCategoryChart, and SalesOverviewChart components. Removed imports and usages of these components from AnalyticsPage."

--- src/pages/AnalyticsPage.jsx
+++ src/pages/AnalyticsPage.jsx
@@ -2,8 +2,7 @@ import Header from "../components/common/Header";
 
 import OverviewCards from "../components/analytics/OverviewCards";
 import RevenueChart from "../components/analytics/RevenueChart";
-import ChannelPerformance from "../components/analytics/ChannelPerformance";
-import ProductPerformance from "../components/analytics/ProductPerformance";
+import ChannelPerformance from "../components/analytics/ChannelPerfomance";
 import UserRetention from "../components/analytics/UserRetention";
 import CustomerSegmentation from "../components/analytics/CustomerSegmentation";
 import AIPoweredInsights from "../components/analytics/AIPoweredInsights";
